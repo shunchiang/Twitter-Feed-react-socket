9b47ae38c166aa3dbd5df729993e9cdb9b33cd8a
Shun Chiang <shunchiang@Shuns-MacBook-Pro.local>
2020-04-01 01:08:15 -0500

    first commit

--- src/components/TweetList.js
+++ src/components/TweetList.js
@@ -1,132 +1,176 @@
-import React from 'react';
-import { CSSTransitionGroup } from 'react-transition-group';
+import React from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import socketIOClient from "socket.io-client";
-import CardComponent from './CardComponent';
-
+import CardComponent from "./CardComponent";
 
 class TweetList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { items: [], searchTerm: "JavaScript" };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleKeyPress = this.handleKeyPress.bind(this);
-    this.handleResume = this.handleResume.bind(this);
-    this.handlePause = this.handlePause.bind(this);
+    this.state = {
+      items: [],
+      renderedThings: [],
+      itemsRendered: 0,
+      searchTerm: "COVID19",
+      inProp: true
+    };
   }
 
-  handleChange(event) {
+  handleChange = event => {
     this.setState({ searchTerm: event.target.value });
-  }
+  };
 
-  handleKeyPress(event) {
-    if (event.key === 'Enter') {
+  handleKeyPress = event => {
+    if (event.key === "Enter") {
       this.handleResume();
     }
-  }
+  };
 
-  handleResume() {
+  handleResume = () => {
     let term = this.state.searchTerm;
-    fetch("/setSearchTerm",
-      {
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ term })
-      })
-  }
-
-  handlePause(event) {
-    fetch("/pause",
-      {
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-  }
-
-componentDidMount() {
-  const socket = socketIOClient('http://localhost:3000/');
-
-  socket.on('connect', () => {
-    console.log("Socket Connected");
-    socket.on("tweets", data => {
-      console.info(data);
-      let newList = [data].concat(this.state.items.slice(0, 15));
-      this.setState({ items: newList });
+    fetch("/setSearchTerm", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ term })
     });
-  });
-  socket.on('disconnect', () => {
-    socket.off("tweets")
-    socket.removeAllListeners("tweets");
-    console.log("Socket Disconnected");
-  });
-}
+  };
 
+  handlePause = event => {
+    fetch("/pause", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+  };
+  ScheduleNextUpdate = () => {
+    this.timer = setTimeout(this.updateRendered, 2000);
+  };
+
+  updateRendered = () => {
+    const itemsRendered = this.state.itemsRendered;
+    const updatedState = {
+      renderedThings:
+        this.state.items.length > 1
+          ? this.state.renderedThings.concat(
+              this.state.items[this.state.itemsRendered]
+            )
+          : /* [this.state.items[this.state.itemsRendered]].concat(
+              this.state.renderedThings.slice(0, 15)
+            ) */
+            [],
+      itemsRendered: this.state.items.length < 16 ? itemsRendered + 1 : 0
+    };
+    this.setState(updatedState);
+    updatedState.itemsRendered < this.state.items.length &&
+      this.ScheduleNextUpdate();
+  };
+
+  componentDidMount() {
+    this.ScheduleNextUpdate();
+
+    const socket = socketIOClient("http://localhost:3000/");
+
+    socket.on("connect", () => {
+      console.log("Socket Connected");
+      socket.on("tweets", data => {
+        console.info(data);
+
+        let newList = [data].concat(this.state.items.slice(0, 15));
+        this.setState({ items: newList });
+      });
+    });
+    socket.on("disconnect", () => {
+      socket.off("tweets");
+      socket.removeAllListeners("tweets");
+      console.log("Socket Disconnected");
+    });
+  }
+  componentWillUnmount() {
+    clearTimeout(this.timer);
+  }
 
   render() {
     let items = this.state.items;
+    console.log(items);
+    console.log(this.state.renderedThings);
 
-    let itemsCards = <CSSTransitionGroup
-      transitionName="example"
-      transitionEnterTimeout={500}
-      transitionLeaveTimeout={300}>
-      {items.map((x, i) =>
-        <CardComponent key={i} data={x} />
-      )}
-    </CSSTransitionGroup>;
-
-    let searchControls =
+    let searchControls = (
       <div>
-        <input id="email" type="text" className="validate" value={this.state.searchTerm} onKeyPress={this.handleKeyPress} onChange={this.handleChange} />
+        <input
+          id="email"
+          type="text"
+          className="validate"
+          value={this.state.searchTerm}
+          onKeyPress={this.handleKeyPress}
+          onChange={this.handleChange}
+        />
         <label htmlFor="email">Search</label>
       </div>
+    );
 
-    let filterControls = <div>
-      <a className="btn-floating btn-small waves-effect waves-light pink accent-2" style={controlStyle} onClick={this.handleResume}><i className="material-icons">play_arrow</i></a>
-      <a className="btn-floating btn-small waves-effect waves-light pink accent-2" onClick={this.handlePause} ><i className="material-icons">pause</i></a>
-      <p>
-        <input type="checkbox" id="test5" />
-        <label htmlFor="test5">Retweets</label>
-      </p>
-    </div>
-
-    let controls = <div>
-      {
-        items.length > 0 ? filterControls : null
-      }
-    </div>
-
-    let loading = <div>
-      <p className="flow-text">Listening to Streams</p>
-      <div className="progress lime lighten-3">
-        <div className="indeterminate pink accent-1"></div>
+    let filterControls = (
+      <div>
+        <a
+          className="btn-floating btn-small waves-effect waves-light pink accent-2"
+          style={controlStyle}
+          onClick={this.handleResume}
+        >
+          <i className="material-icons">play_arrow</i>
+        </a>
+        <a
+          className="btn-floating btn-small waves-effect waves-light pink accent-2"
+          onClick={this.handlePause}
+        >
+          <i className="material-icons">pause</i>
+        </a>
+        <p>
+          <input type="checkbox" id="test5" />
+          <label htmlFor="test5">Retweets</label>
+        </p>
       </div>
-    </div>
+    );
+
+    let controls = <div>{items.length > 0 ? filterControls : null}</div>;
+
+    let loading = (
+      <div>
+        <p className="flow-text">Listening to Streams</p>
+        <div className="progress lime lighten-3">
+          <div className="indeterminate pink accent-1"></div>
+        </div>
+      </div>
+    );
 
     return (
       <div className="row">
         <div className="col s12 m4 l4">
           <div className="input-field col s12">
             {searchControls}
-            {
-              items.length > 0 ? controls : null
-            }
+            {items.length > 0 ? controls : null}
           </div>
         </div>
         <div className="col s12 m4 l4">
           <div>
-            {
-              items.length > 0 ? itemsCards : loading
-            }
-
+            {this.state.renderedThings.length > 1 ? (
+              <TransitionGroup className="flex">
+                {this.state.renderedThings.map((x, i) => (
+                  <CSSTransition
+                    in={this.state.inProp}
+                    classNames="example"
+                    timeout={1000}
+                  >
+                    <CardComponent key={i} data={x} />
+                  </CSSTransition>
+                ))}
+              </TransitionGroup>
+            ) : (
+              loading
+            )}
           </div>
-
         </div>
-        <div className="col s12 m4 l4">
-        </div>
+        <div className="col s12 m4 l4"></div>
       </div>
     );
   }
--- src/server/routes/tweets.js
+++ src/server/routes/tweets.js
@@ -1,79 +1,84 @@
-const Twitter = require('twitter');
+const Twitter = require("twitter");
+require("dotenv").config({ path: "../../.env" });
 
 module.exports = (app, io) => {
-    let twitter = new Twitter({
-        consumer_key: process.env.TWITTER_CONSUMER_KEY,
-        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-        access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-    });
+  let twitter = new Twitter({
+    consumer_key: "ZRCoumSo8cjRRwvD5tutfWdBU",
+    consumer_secret: "pv2wh5UpT9gPXuZaWkHIsFLOOyqayrBYyn3rL0iiEBOWnuGyvs",
+    access_token_key: "1251960258-zhaJpqGprw8eUkZJCLRrQwSpFaFX1uZCDxJqq5J",
+    access_token_secret: "P97Gkum3Qj0228ozm5ShWUwbu2JS24cqYkDq6iChwDs3R"
+  });
 
-    let socketConnection;
-    let twitterStream;
+  let socketConnection;
+  let twitterStream;
 
-    app.locals.searchTerm = 'JavaScript'; //Default search term for twitter stream.
-    app.locals.showRetweets = false; //Default
+  app.locals.searchTerm = "COVID19"; //Default search term for twitter stream.
+  app.locals.showRetweets = false; //Default
 
-    /**
-     * Resumes twitter stream.
-     */
-    const stream = () => {
-        console.log('Resuming for ' + app.locals.searchTerm);
-        twitter.stream('statuses/filter', { track: app.locals.searchTerm }, (stream) => {
-            stream.on('data', (tweet) => {
-                sendMessage(tweet);
-            });
-
-            stream.on('error', (error) => {
-                console.log(error);
-            });
-
-            twitterStream = stream;
+  /**
+   * Resumes twitter stream.
+   */
+  const stream = () => {
+    console.log("Resuming " + app.locals.searchTerm);
+    twitter.stream(
+      "statuses/filter",
+      { track: app.locals.searchTerm },
+      stream => {
+        stream.on("data", tweet => {
+          sendMessage(tweet);
         });
+
+        stream.on("error", error => {
+          console.log(error);
+        });
+
+        twitterStream = stream;
+      }
+    );
+  };
+
+  /**
+   * Sets search term for twitter stream.
+   */
+  app.post("/setSearchTerm", (req, res) => {
+    let term = req.body.term;
+    app.locals.searchTerm = term;
+    twitterStream.destroy();
+    stream();
+  });
+
+  /**
+   * Pauses the twitter stream.
+   */
+  app.post("/pause", (req, res) => {
+    console.log("Pause");
+    twitterStream.destroy();
+  });
+
+  /**
+   * Resumes the twitter stream.
+   */
+  app.post("/resume", (req, res) => {
+    console.log("Resume");
+    stream();
+  });
+
+  //Establishes socket connection.
+  io.on("connection", socket => {
+    socketConnection = socket;
+    stream();
+    socket.on("connection", () => console.log("Client connected"));
+    socket.on("disconnect", () => console.log("Client disconnected"));
+  });
+
+  /**
+   * Emits data from stream.
+   * @param {String} msg
+   */
+  const sendMessage = msg => {
+    if (msg.text.includes("RT")) {
+      return;
     }
-
-    /**
-     * Sets search term for twitter stream.
-     */
-    app.post('/setSearchTerm', (req, res) => {
-        let term = req.body.term;
-        app.locals.searchTerm = term;
-        twitterStream.destroy();
-        stream();
-    });
-
-    /**
-     * Pauses the twitter stream.
-     */
-    app.post('/pause', (req, res) => {
-        console.log('Pause');
-        twitterStream.destroy();
-    });
-
-    /**
-     * Resumes the twitter stream.
-     */
-    app.post('/resume', (req, res) => {
-        console.log('Resume');
-        stream();
-    });
-
-    //Establishes socket connection.
-    io.on("connection", socket => {
-        socketConnection = socket;
-        stream();
-        socket.on("connection", () => console.log("Client connected"));
-        socket.on("disconnect", () => console.log("Client disconnected"));
-    });
-
-    /**
-     * Emits data from stream.
-     * @param {String} msg 
-     */
-    const sendMessage = (msg) => {
-        if (msg.text.includes('RT')) {
-            return;
-        }
-        socketConnection.emit("tweets", msg);
-    }
+    socketConnection.emit("tweets", msg);
+  };
 };
